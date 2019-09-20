040bfa7a68babf94649a41b523424c8b4a1625b6
ProbeDream <Gump1016@163.com>
2019-09-20 21:19:44 +0800

    添加对应的登录模块样式!

--- src/components/UserDialog.jsx
+++ src/components/UserDialog.jsx
@@ -1,4 +1,5 @@
 import React,{Component} from "react";
+import "./UserDialog.css"
 
 class UserDialog extends Component{
     render() {
@@ -6,22 +7,31 @@ class UserDialog extends Component{
             <div className="UserDialogWrapper">
                 <div className="UserDialog">
                     <nav>
-                        <input type="radio"/>
-                        <input type="radio"/>
+                        <input type="radio"/>注册
+                        <input type="radio"/>登录
                     </nav>
-                </div>
 
-                <div className="panel">
-                    <form className="signUp">
-                        <div><label className="row">用户名</label><input type="text"/></div>
-                        <div><label className="row">密&nbsp;&nbsp;&nbsp;&nbsp;码</label><input type="password"/></div>
-                        <div><label className="row actions">注册</label><button type="submit">密码</button></div>
-                    </form>
-                    <form className="signIn">
-                        <div><label className="row">用户名</label><input type="text"/></div>
-                        <div><label className="row">密&nbsp;&nbsp;&nbsp;&nbsp;码</label><input type="password"/></div>
-                        <div><label className="row">登录</label><button type="submit">登录</button></div>
-                    </form>
+                    <div className="panel">
+                        <form className="signUp">
+                            <div><label className="row">用户名</label><input type="text"/></div>
+                            <div><label className="row">密&nbsp;&nbsp;&nbsp;&nbsp;码</label><input type="password"/></div>
+                            <div>
+                                <class className="row actions">
+                                    <button type="submit">注册</button>
+                                </class>
+                            </div>
+                        </form>
+                        <form className="signIn">
+                            <div><label className="row">用户名</label><input type="text"/></div>
+                            <div><label className="row">密&nbsp;&nbsp;&nbsp;&nbsp;码</label><input type="password"/></div>
+                             <div>
+                                 <class className="row">
+                                     <button type="submit">登录</button>
+                                 </class>
+                            </div>
+                        </form>
+                    </div>
+
                 </div>
             </div>
         );
