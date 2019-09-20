4d82ee43ef0f1d08c2c5bb971d6c52a1bec720a1
ProbeDream <Gump1016@163.com>
2019-09-20 11:59:52 +0800

    添加Input的键盘事件!

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -2,8 +2,13 @@ import React,{Component} from "react";
 class TodoInput extends Component{
     render(){
         return (
-            <input type='text'  defaultValue={this.props.content}/>
+            <input type='text'  defaultValue={this.props.content} onKeyPress={this.submit}/>
         );
+    }
+    submit(e){
+        if (e.key === 'Enter'){
+            console.log(`用户提交了具体数据为:${e.target.value}`);
+        }
     }
 }
 export default TodoInput;
