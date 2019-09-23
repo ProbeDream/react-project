4c6de4202f6c3611ec00c861f10a09bf835706af
ProbeDream <Gump1016@163.com>
2019-09-23 15:19:31 +0800

    对对应的组件进行抽离成一个组件!

---
+++ src/components/ForgotPasswordForm.jsx
@@ -0,0 +1,21 @@
+import React,{Component} from "react";
+class ForgotPasswordForm extends Component{
+    render() {
+        return (
+            <div className="forgotPassword">
+                <h3>重置密码</h3>
+                <form  className="forgotPassword" onSubmit={this.props.submit}>
+                    <div className="row">
+                        <label htmlFor="">邮箱</label>
+                        <input type="text" value={this.props.formData.email} onChange={this.props.onChange.bind(null,'email')}/>
+                    </div>
+                    <div className="row actions">
+                        <button type="submit">发送重置邮件</button>
+                        <a href="#" onClick={this.props.onSignIn}>返回登录</a>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+export default ForgotPasswordForm;
